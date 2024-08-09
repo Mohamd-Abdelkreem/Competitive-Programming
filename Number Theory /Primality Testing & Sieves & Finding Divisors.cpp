4b3e811f70cd83b68b78c68b41b70d62be2b1aa7
Mohamed Abd El-Kareem <130367629+Mohamd-Abdelkreem@users.noreply.github.com>
2024-08-09 06:32:17 +0300

    Update Primality Testing & Sieves & Finding Divisors.cpp

--- Number Theory /Primality Testing & Sieves & Finding Divisors.cpp
+++ Number Theory /Primality Testing & Sieves & Finding Divisors.cpp
@@ -68,8 +68,8 @@ ostream &operator<<(ostream &out, const vector<T> &v) {
 
 bool IsPrime(ll n) {
     //TC = O(log n)
-    if (n % 2 == 0)return true;
-    if ((n < 2) || (n % 2 == 0))return false;
+    if (n == 2)return true;
+    if (n <= 1 || n % 2 == 0)return false;
 
     for (ll i = 3; i * i <= n; i += 2) {
         if (n % i == 0) return false;
